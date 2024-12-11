ba48be708d6644b13f1a8e5c25fa203a66562499
Egor Skabitski <130250057+EgorSkab@users.noreply.github.com>
2024-12-11 09:53:21 +0300

    Update filler.py

--- filler.py
+++ filler.py
@@ -17,6 +17,7 @@ def add_data_to_table(file_path : str, table_name : str, columns : list, data : 
                 data_list += str(dat[i]) + ", "
             data_list = data_list[:-2] + ")"
         cursor.execute("INSERT INTO " + table_name + " " + column_list + " VALUES " + data_list + "WHERE " + condition)
+        connection.commit()
         connection.close()
         return
     except sqlite3.Error as error:
@@ -27,6 +28,7 @@ def erase_data_from_table(file_path : str, table_name : str, condition : str = '
         connection = sqlite3.connect(file_path)
         cursor = connection.cursor()
         cursor.execute("DELETE FROM " + table_name + " WHERE " + condition)
+        connection.commit()
         connection.close()
         return
     except sqlite3.Error as error:
@@ -37,6 +39,7 @@ def modify_data_in_table(file_path : str, table_name : str, column : str, value 
         connection = sqlite3.connect(file_path)
         cursor = connection.cursor()
         cursor.execute("UPDATE " + table_name + " SET " + column + "=" + value + " WHERE " + condition)
+        connection.commit()
         connection.close()
         return
     except sqlite3.Error as error:
@@ -47,6 +50,7 @@ def set_data_none_in_table(file_path : str, table_name : str, column : str, cond
         connection = sqlite3.connect(file_path)
         cursor = connection.cursor()
         cursor.execute("UPDATE " + table_name + " SET " + column + "=NULL WHERE " + condition)
+        connection.commit()
         connection.close()
         return
     except sqlite3.Error as error:
