d04722f15ed2c3099c75bef25ad1b542d2cc738b
Egor Skabitski <130250057+EgorSkab@users.noreply.github.com>
2024-12-15 15:10:08 +0300

    Update viever.py

--- viever.py
+++ viever.py
@@ -70,7 +70,7 @@ def get_structure(file_path: str) -> str:
 
 def main():
     parser = argparse.ArgumentParser(description="Database Viewer")
-    parser.add_argument('function', choices=['check_login', 'check_access', 'get_all_databases', 'get_database_tables', 'get_table_columns', 'get_table_data', 'get_structure'], help='Function to execute')
+    parser.add_argument('function', choices=['check_login', 'check_access', 'get_avalable_databases', 'get_database_tables', 'get_table_columns', 'get_table_data', 'get_structure'], help='Function to execute')
     parser.add_argument('params', nargs='*', help='Parameters for the function')
     args = parser.parse_args()
 
@@ -95,7 +95,6 @@ def main():
             print("Usage: python viewer.py get_all_databases <login>")
         else:
             login = args.params[0]
-            database_id = int(args.params[1])
             result = get_avalable_databases(login)
             print(result)
     elif args.function == 'get_database_tables':
