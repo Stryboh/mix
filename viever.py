c56d6e168e68e0a046793f3674448c16255ff909
EgorSkab <7933556egor@gmail.com>
2024-12-15 14:18:24 +0300

    Updated viewer.py

--- viever.py
+++ viever.py
@@ -21,6 +21,13 @@ def check_access(login: str, database_id: int) -> bool:
     else:
         return False
 
+def get_all_databases() -> list:
+    connection = sqlite3.connect("credentials.db")
+    cursor = connection.cursor()
+    databases = cursor.execute(f"SELECT * FROM Databases").fetchall()
+    connection.close()
+    return databases
+
 def get_database_tables(file_path: str) -> list | None:
     connection = sqlite3.connect(file_path)
     cursor = connection.cursor()
@@ -63,56 +70,56 @@ def get_structure(file_path: str) -> str:
 
 def main():
     parser = argparse.ArgumentParser(description="Database Viewer")
-    parser.add_argument('function', choices=['check_login', 'check_access', 'get_database_tables', 'get_table_columns', 'get_table_data', 'get_structure'], help='Function to execute')
+    parser.add_argument('function', choices=['check_login', 'check_access', 'get_all_databases', 'get_database_tables', 'get_table_columns', 'get_table_data', 'get_structure'], help='Function to execute')
     parser.add_argument('params', nargs='*', help='Parameters for the function')
     args = parser.parse_args()
 
     if args.function == 'check_login':
         if len(args.params) != 2:
-            return("Usage: python viewer.py check_access <login> <database_id>")
+            print("Usage: python viewer.py check_access <login> <database_id>")
         else:
             login = args.params[0]
             password = args.params[1]
             result = check_login(login, password)
-            return(result)
+            print(result)
     elif args.function == 'check_access':
         if len(args.params) != 2:
-            return("Usage: python viewer.py check_access <login> <database_id>")
+            print("Usage: python viewer.py check_access <login> <database_id>")
         else:
             login = args.params[0]
             database_id = int(args.params[1])
             result = check_access(login, database_id)
-            return(result)
+            print(result)
     elif args.function == 'get_database_tables':
         if len(args.params) != 1:
-            return("Usage: python viewer.py get_database_tables <file_path>")
+            print("Usage: python viewer.py get_database_tables <file_path>")
         else:
             file_path = args.params[0]
             result = get_database_tables(file_path)
-            return(result)
+            print(result)
     elif args.function == 'get_table_columns':
         if len(args.params) != 2:
-            return("Usage: python viewer.py get_table_columns <file_path> <table_name>")
+            print("Usage: python viewer.py get_table_columns <file_path> <table_name>")
         else:
             file_path = args.params[0]
             table_name = args.params[1]
             result = get_table_columns(file_path, table_name)
-            return(result)
+            print(result)
     elif args.function == 'get_table_data':
         if len(args.params) != 2:
-            return("Usage: python viewer.py get_table_data <file_path> <table_name>")
+            print("Usage: python viewer.py get_table_data <file_path> <table_name>")
         else:
             file_path = args.params[0]
             table_name = args.params[1]
             result = get_table_data(file_path, table_name)
-            return(result)
+            print(result)
     elif args.function == 'get_structure':
         if len(args.params) != 1:
-            return("Usage: python viewer.py get_structure <file_path>")
+            print("Usage: python viewer.py get_structure <file_path>")
         else:
             file_path = args.params[0]
             result = get_structure(file_path)
-            return(result)
+            print(result)
 
 if __name__ == '__main__':
     main()
