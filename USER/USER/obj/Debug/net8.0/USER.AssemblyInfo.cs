9bc8a0608efc424c64a3ec02323bf66903ba1152
stryboh <daniladaniladanilin@gmail.com>
2024-12-20 11:13:44 +0300

    RELEASE2

--- USER/USER/obj/Debug/net8.0/USER.AssemblyInfo.cs
+++ USER/USER/obj/Debug/net8.0/USER.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("USER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7805fd91fd0e8c82a3915feb2c5a7b1abfa90875")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8bfa43311554f347280b99ffdfa7a0a23f80a88")]
 [assembly: System.Reflection.AssemblyProductAttribute("USER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("USER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
